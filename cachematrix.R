4c4d322739120918e655a57bb4d6cb74c1959cab
JLF <julien@lafrance.io>
2016-03-26 14:48:42 +0100

    V3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
 ## JLF - March 2016 
-## Store in cache the Inverse of a matrix tto make available later on
+## Store in cache the Inverse of a matrix to make available later on
 
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
+  inv <- NULL
   
   ## Define different methods to play with a matrix
+  ## @x: a square invertible matrix
+  ## return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is used as the input to cacheSolve()
   
   ## Store the matrix in the cache
   
   setMatrix <- function(matrix = matrix()){
+    
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    
     x <<- matrix
     inverse <- NULL
   }
@@ -38,13 +50,34 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Otherwise, it calculates the inverse Matrix and caches it
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x' 
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
   
-  if(is.null(x$getInverse())){
+  inv = x$getInverse()
+  
+  if(is.null(inv))  {
+    # inverse matrix not existing, calculates the inverse 
     print("Inverse matrix does not exists in cache. Computing...") 
-    x$setInverse(solve(x$get()))
+    inv <- solve(x$get())
+    # sets the value of the inverse in the cache via the setInverse function.
+    x$setInverse(inv)
     print("Inverse matrix computed and cached.") 
-  }else {print("Inverse Matrix from cache")}
+    } 
+  else {
+    # get it from the cache and skips the computation.
+    print("Inverse matrix from cache")
+    }
   
-  x$getInverse()
+  return(inv)
 }
+
+## Data to play with and to test
+## matrixplay <- matrix(data = c(4,2,7,8), nrow = 2, ncol = 2)
+## matrixplay2 <- makeCacheMatrix(matrixplay)
+## Not cached
+## cacheSolve(matrixplay2)
+## cached 
+## cacheSolve(matrixplay2)
+## Result should be the identitiy matrix
+## matrixplay  %*% cacheSolve(matrixplay2)
+
